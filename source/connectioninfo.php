8642bc7061b3fe06d60a9a20a32e2379a5ca9077
Derek Eder <derek.eder@gmail.com>
2011-09-27 11:54:12 -0500

    added connectioninfo.php back in

---
+++ source/connectioninfo.php
@@ -0,0 +1,14 @@
+<?php
+
+class ConnectionInfo {
+
+  public static $google_username = "xxxxxx";
+  public static $google_password = "xxxxxx";
+
+  public static $view_uid = "7nii-7srd";
+  public static $data_site = "data.cityofchicago.org";
+  public static $app_tokenn = "xxxxxx";
+  public static $fusionTableId = 1614852;
+  
+}
+?>
