6799acf0b4b3034588714b36ffb1173df003900c
Derek Eder <derek.eder@gmail.com>
2011-09-27 11:55:40 -0500

    added comments to connection info variables

--- source/connectioninfo.php
+++ source/connectioninfo.php
@@ -2,12 +2,13 @@
 
 class ConnectionInfo {
 
+  //Google account that owns the Fusion Table
   public static $google_username = "xxxxxx";
   public static $google_password = "xxxxxx";
 
-  public static $view_uid = "7nii-7srd";
-  public static $data_site = "data.cityofchicago.org";
-  public static $app_tokenn = "xxxxxx";
+  public static $view_uid = "7nii-7srd"; //Chicago's building data: http://data.cityofchicago.org/Service-Requests/311-Service-Requests-Vacant-and-Abandoned-Building/7nii-7srd
+  public static $data_site = "data.cityofchicago.org"; //Chicago's data portal
+  public static $app_tokenn = "xxxxxx"; //register with Socrata here: http://dev.socrata.com/authentication
   public static $fusionTableId = 1614852;
   
 }
